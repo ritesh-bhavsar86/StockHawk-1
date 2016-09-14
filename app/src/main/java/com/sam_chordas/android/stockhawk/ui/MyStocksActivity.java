c90b04a008b3c19fc5d406d46f0d9965379d457c
ege <egek92@yandex.com>
2016-09-14 17:14:37 +0300

    hard coded string added

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
@@ -125,7 +125,7 @@ public class MyStocksActivity extends AppCompatActivity implements LoaderManager
                       new String[] { input.toString() }, null);
                   if (c.getCount() != 0) {
                     Toast toast =
-                        Toast.makeText(MyStocksActivity.this, "This stock is already saved!",
+                        Toast.makeText(MyStocksActivity.this, getString(R.string.toast),
                             Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.CENTER, Gravity.CENTER, 0);
                     toast.show();
